a94b908d4bd7d615e1f39248fdafe67c35740847
Joel Purra <code+github@joelpurra.com>
2010-11-18 20:40:43 +0000

    Leeet's just not scale DPI stuff right now
    git-svn-id: svn://localhost/WellCompareDir/trunk@992 e549bf12-f090-ff4b-a60d-830663ca835f

--- WellCompareDir.WPF/MainWindowView.xaml.cs
+++ WellCompareDir.WPF/MainWindowView.xaml.cs
@@ -14,76 +14,11 @@ using System.Windows.Shapes;
 
 namespace WellCompareDir.WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindowView.xaml
-    /// </summary>
     public partial class MainWindowView : Window
     {
         public MainWindowView()
         {
             InitializeComponent();
-
-            leftImage.SizeChanged += new SizeChangedEventHandler(image_SizeChanged);
-            rightImage.SizeChanged += new SizeChangedEventHandler(image_SizeChanged);
-        }
-
-        void image_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            SetStretchMode(sender);
-        }
-
-        private void SetStretchMode(object sender)
-        {
-            Image image = sender as Image;
-
-            if (image != null)
-            {
-                BitmapSource source = (BitmapSource)image.Source;
-
-                if (source != null)
-                {
-                    double WPF_DPI_BASE = 96;
-
-                    double imageScaleX = source.DpiX / WPF_DPI_BASE;
-                    double imageScaleY = source.DpiY / WPF_DPI_BASE;
-
-                    image.RenderTransform = new ScaleTransform((imageScaleX / this.ScreenScale.ScaleX), (imageScaleY / this.ScreenScale.ScaleY));
-
-                    //FrameworkElement parent = image.Parent as FrameworkElement;
-
-                    //if (source.PixelWidth < parent.ActualWidth
-                    //    || source.PixelHeight < parent.ActualHeight)
-                    //{
-                    //    image.Stretch = Stretch.None;
-                    //}
-                    //else
-                    //{
-                    //    image.Stretch = Stretch.Uniform;
-                    //}
-                }
-                else
-                {
-                    image.RenderTransform = Transform.Identity;
-                }
-            }
-        }
-
-        private ScaleTransform screenScale = null;
-        private ScaleTransform ScreenScale
-        {
-            get
-            {
-                if (screenScale == null)
-                {
-                    Matrix m = PresentationSource.FromVisual(Application.Current.MainWindow).CompositionTarget.TransformToDevice;
-                    double screenScaleX = m.M11;
-                    double screenScaleY = m.M22;
-
-                    screenScale = new ScaleTransform(screenScaleX, screenScaleY);
-                }
-
-                return screenScale;
-            }
         }
     }
 }
